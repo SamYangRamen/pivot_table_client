f2ea971bb3dd54b08092c32029693bb4ea9f961a
seongbo_kim <seongbo_kim@tmax.co.kr>
2021-11-04 09:51:44 +0900

    Add Remark

--- pivot_table/src/Root.tsx
+++ pivot_table/src/Root.tsx
@@ -7,6 +7,11 @@ import RootStore from './RootStore';
 import StoreProvider from './StoreProvider';
 import ValueStore from './ValueStore';
 
+/*  RootStore는, 프로그램 실행 내내 메모리 상에서 살아있어야 하고
+    모든 컴포넌트들에서 공유 및 사용될 수 있어야 하는 데이터들을 저장하는 인터페이스임
+    본 코드에서는 RootStore를 초기화하여 다른 컴포넌트들을 감싸는 형태로 만듦으로써
+    모든 컴포넌트들에서 접근 가능하도록 작성한 것임
+*/
 const Root: React.FC = () => {
     const [store] = useState<RootStore>({ valueStore: new ValueStore });
 
